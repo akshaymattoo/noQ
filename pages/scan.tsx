0371c28437ef6597b3158f5c7c38bc4b43e8ab4d
akshay <amattoo@us.ibm.com>
2021-05-25 21:30:57 -0700

    added page scan

---
+++ pages/scan.tsx
@@ -0,0 +1,22 @@
+import Webcam from "react-webcam";
+
+const Scan = () => {
+    const videoConstraints = {
+        width: { min: 480 },
+        height: { min: 720 },
+        aspectRatio: 0.6666666667,
+        facingMode: "forward"
+      };
+    return (
+        <div>
+            <h1>hello</h1>
+            <Webcam 
+                videoConstraints={videoConstraints} 
+                width={480} 
+                height={720}
+                />
+        </div>
+    )
+}
+
+export default Scan
