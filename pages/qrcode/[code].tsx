a146ec38a48948c35a087269877ab6f642a84014
akshay <amattoo@us.ibm.com>
2021-05-30 20:59:41 -0700

    fixed error

--- pages/qrcode/[code].tsx
+++ pages/qrcode/[code].tsx
@@ -19,7 +19,7 @@ export default function Code({ data }) {
             </Head>
 
             <main className={styles.main}>
-            <QRCode value={`${data.name}${data.number}`} />
+            <QRCode {...data} />
             </main>
         </div>
     )
